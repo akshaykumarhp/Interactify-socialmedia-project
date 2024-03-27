0467c5a068cc3915be77ddfdff8393d3f502d56b
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-03-27 00:02:24 -0400

    Changes in home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
      
     return(
         <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgcolor lg:rounded-lg h-screen overflow-hidden'>
-            <TopBar/>
+            
         </div>
     )
 }
