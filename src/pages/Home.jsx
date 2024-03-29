b56eb9ec671fe92895592eb1722983b4fa376ba5
Akshay Kumar Huliyar Prabhakara <akshaykumarhp11@gmail.com>
2024-03-29 19:07:04 -0400

    Top Bar added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { TopBar } from '../components';
 
 
 const Home = () => {
@@ -10,6 +11,7 @@ const Home = () => {
      
     return(
         <div className='home w-full px-0 lg:px-10 pb-20 2xl:px-40 bg-bgcolor lg:rounded-lg h-screen overflow-hidden'>
+            <TopBar/>
             
         </div>
     )
